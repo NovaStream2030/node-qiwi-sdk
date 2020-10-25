0601c0325f3d58e4efd8b338564376dbf4fc3b8a
MagicFun1241 <nganzikov@gmail.com>
2020-10-25 18:20:29 +0400

    Code style improvements

--- src/interfaces/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./IBill";
-export * from "./IAmount";
-export * from "./IOptions";
-export * from "./ICustomer";
-export * from "./IBillResponse";
-export * from "./IPaymentFormParams";
-export * from "./ICheckNotificationSignature";
--- src/interfaces/options.ts
+++ src/interfaces/options.ts
@@ -1,4 +1,4 @@
-export interface IOptions {
+export default interface Options {
     secretKey: string;
     publicKey: string;
     url?: string;
